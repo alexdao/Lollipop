6c72962a19b8e9803b424d1e18f3ecf5fce27216
Alex Dao <azd2@duke.edu>
2015-09-20 04:01:48 -0400

    Set up rest client android-side

---
+++ app/src/main/java/com/lumivote/lollipop/api/UploadRESTAdapter.java
@@ -0,0 +1,68 @@
+package com.lumivote.lollipop.api;
+
+import android.util.Log;
+
+import com.lumivote.lollipop.bus.BusProvider;
+import com.squareup.otto.Bus;
+
+import java.io.File;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import retrofit.http.Multipart;
+import retrofit.http.POST;
+import retrofit.http.Part;
+import retrofit.mime.TypedFile;
+
+/**
+ * Created by alex on 9/20/15.
+ */
+public class UploadRESTAdapter {
+
+    private static final String API_URL = "http://lumivote.appspot.com/api";
+    private static final UploadRESTAdapter restClient = new UploadRESTAdapter();
+
+    private Bus eventBus;
+
+    private UploadRESTAdapter() {
+    }
+
+    public static UploadRESTAdapter getInstance() {
+        restClient.eventBus = BusProvider.getInstance();
+        restClient.eventBus.register(restClient);
+        return restClient;
+    }
+
+    public interface UploadService {
+        @Multipart
+        @POST("/upload")
+        void upload(@Part("myfile")TypedFile file,
+                    @Part("description") String description,
+                    Callback<String> cb);
+    }
+
+    public void uploadImage(String path) {
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint(API_URL)
+                .build();
+
+        UploadService service = restAdapter.create(UploadService.class);
+
+        TypedFile typedFile = new TypedFile("multipart/form-data", new File(path));
+        String description = "hello, this is description speaking";
+
+        service.upload(typedFile, description, new Callback<String>() {
+            @Override
+            public void success(String s, Response response) {
+                Log.e("Upload", "success");
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.e("Upload", "error");
+            }
+        });
+    }
+}
